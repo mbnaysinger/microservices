71f6d49d0415391d0f55daebb9d8de09e123f4f9
mbnaysinger <maikenborges@gmail.com>
2024-02-26 21:54:18 -0300

    class_part

--- notification/src/main/java/com/store/notification/service/impl/EmailServiceImpl.java
+++ notification/src/main/java/com/store/notification/service/impl/EmailServiceImpl.java
@@ -1,6 +1,19 @@
 package com.store.notification.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailServiceImpl extends GenericServiceImpl{
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private static String mailFrom;
+
     public static String convertToObject(String message){
         return "";
     }
